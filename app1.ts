ab442d460747cd9dd0accc0ec6c57063a31096c0
Faiz <faiz.ggmu@gmail.com>
2018-08-08 17:24:19 +0500

    No Implicit Any

--- app1.ts
+++ app1.ts
@@ -3,3 +3,4 @@ let myNumber1:number=28;
 //myName1=30; although we cant do this in typescript this would throw an error while compiled but
 //still this would be converted into js file
 // because noemitonerror is by default false which lets js file to be compiled
+// noImplicitAny is set to false in tsconfig file which allows to initialize variable without any type
