a8b01b6f9998fa40c741e6d0929e570a3bdf2cac
Faiz <faiz.ggmu@gmail.com>
2018-08-08 17:20:53 +0500

    How code gets compiled

---
+++ app1.js
@@ -0,0 +1,5 @@
+"use strict";
+var myName1 = "Faiz";
+var myNumber1 = 28;
+//myName1=30; although we cant do this in typescript this would throw an error while compiled but
+//still this would be converted into js file
---
+++ app1.ts
@@ -0,0 +1,5 @@
+let myName1:string = "Faiz";
+let myNumber1:number=28;
+//myName1=30; although we cant do this in typescript this would throw an error while compiled but
+//still this would be converted into js file
+// because noemitonerror is by default false which lets js file to be compiled
