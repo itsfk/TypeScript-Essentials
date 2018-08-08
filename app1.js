d3fefe8cbbe209cd78d53bf8eb4a8a2ddad4d680
Faiz <faiz.ggmu@gmail.com>
2018-08-08 17:38:39 +0500

    Let and Const in TypeScript

--- app1.js
+++ app1.js
@@ -3,3 +3,8 @@ var myName1 = "Faiz";
 var myNumber1 = 28;
 //myName1=30; although we cant do this in typescript this would throw an error while compiled but
 //still this would be converted into js file
+// because noemitonerror is by default false which lets js file to be compiled
+// noImplicitAny is set to false in tsconfig file which allows to initialize variable without any type
+// let and cosnt
+var variable = "Test"; // block scope
+console.log(variable);
--- app1.ts
+++ app1.ts
@@ -4,3 +4,14 @@ let myNumber1:number=28;
 //still this would be converted into js file
 // because noemitonerror is by default false which lets js file to be compiled
 // noImplicitAny is set to false in tsconfig file which allows to initialize variable without any type
+// let and cosnt
+let variable="Test"; // block scope
+console.log(variable);
+variable="Another value"
+console.log(variable);
+
+const max=100;
+console.log(max)
+//max=20 const is not assignable again
+
+
